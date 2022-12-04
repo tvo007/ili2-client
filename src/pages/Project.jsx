423433654cc0114e0762853ad216e40390622c1d
Tim Vo <timtvo714@gmail.com>
2022-12-03 17:11:38 -0800

    media query

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,7 +8,7 @@ const Project = () => {
   return (
     <Layout>
       <Private>
-        <div className="min-h-screen md:px-[11rem]">
+        <div className="min-h-screen xl:px-[11rem]">
           <ProjectHeader />
           <TasksSection />
         </div>
