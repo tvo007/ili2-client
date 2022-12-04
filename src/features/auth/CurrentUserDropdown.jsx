bc6109616242eba22d51a0d1f04bd9dee9c27445
Tim Vo <timtvo714@gmail.com>
2022-12-03 17:22:59 -0800

    drop down user nav styling

--- src/features/auth/CurrentUserDropdown.jsx
+++ src/features/auth/CurrentUserDropdown.jsx
@@ -65,7 +65,11 @@ const CurrentUserDropdown = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-16 mt-4 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items
+          className="absolute right-16 xl:right-52 mt-4 w-56 origin-top-right divide-y 
+        divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 
+        focus:outline-none"
+        >
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
